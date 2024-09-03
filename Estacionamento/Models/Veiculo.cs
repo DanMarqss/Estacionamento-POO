4609a3491dc777d6ef8c0c31b559ee466623cdb0
bankmarqs <danilojmarqs@gmail.com>
2024-09-03 10:35:47 -0400

    Criando Model

---
+++ Estacionamento/Models/Veiculo.cs
@@ -0,0 +1,15 @@
+// Models/Veiculo.cs
+namespace Estacionamento.Models
+{
+    public class Veiculo
+    {
+        public string Placa { get; set; }
+        public DateTime HoraEntrada { get; set; }
+
+        public Veiculo(string placa)
+        {
+            Placa = placa;
+            HoraEntrada = DateTime.Now;
+        }
+    }
+}
