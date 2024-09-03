1f6c08630d58db2525f4b97bc1a47d65576560e6
bankmarqs <danilojmarqs@gmail.com>
2024-09-03 10:32:01 -0400

    Ponto de Entrada da Aplicação

--- Estacionamento/Program.cs
+++ Estacionamento/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program.cs
+using Estacionamento.Models;
+
+namespace Estacionamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estacionamento estacionamento = new Estacionamento();
+
+            while (true)
+            {
+                Console.WriteLine("1. Adicionar Veículo");
+                Console.WriteLine("2. Remover Veículo");
+                Console.WriteLine("3. Listar Veículos");
+                Console.WriteLine("4. Sair");
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.Write("Digite a placa do veículo: ");
+                        string placaAdicionar = Console.ReadLine();
+                        estacionamento.AdicionarVeiculo(placaAdicionar);
+                        break;
+                    case "2":
+                        Console.Write("Digite a placa do veículo: ");
+                        string placaRemover = Console.ReadLine();
+                        estacionamento.RemoverVeiculo(placaRemover);
+                        break;
+                    case "3":
+                        estacionamento.ListarVeiculos();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            }
+        }
+    }
+}
